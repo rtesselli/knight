8add9fcd27b9fe28a228be783c0c2e5c3eb7f1f7
Riccardo Tesselli <riccardo.tesselli@prometeia.com>
2021-05-20 22:41:47 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+import pathlib
+import os.path
+import codecs
+
+here = pathlib.Path(__file__).parent.resolve()
+
+# Get the long description from the README file
+long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+
+def read(rel_path):
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delimiter = '"' if '"' in line else "'"
+            return line.split(delimiter)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
+def read_requirements(path):
+    with open(path, 'r') as file:
+        return file.readlines()
+
+
+setup(
+    name='knight',
+    version=get_version('knight/__init__.py'),
+    description='Knight shortest path',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='',  # TODO
+    author='Riccardo Tesselli',
+    author_email='riccardo.tesselli@gmail.com',
+    package_dir={'': 'knight'},
+    packages=find_packages(where='knight'),
+    python_requires='>=3.9',
+    install_requires=read_requirements('requirements.txt'),
+    extras_require={  # Optional
+        'test': read_requirements('requirements_test.txt'),
+    }
+)
