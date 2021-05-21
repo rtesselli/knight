85f89493e754d4e6ae199a6f699165df12bdc49a
Riccardo Tesselli <riccardo.tesselli@prometeia.com>
2021-05-21 21:54:23 +0200

    scaffolding

--- knight/main.py
+++ knight/main.py
@@ -1,8 +1,12 @@
 from knight.parse import parse_input
+from knight.logic import solve
+from knight.output import output_solutions
 
 
 def main():
     statements = parse_input()
+    results = solve(statements)
+    output_solutions(results)
 
 
 # echo "Input to my program" | python main.py
