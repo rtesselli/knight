005b2ad55f5c69b12afe83a0639bf469ae6e62f1
Riccardo Tesselli <riccardo.tesselli@prometeia.com>
2021-05-22 08:28:25 +0200

    add test

--- tests/test_logic.py
+++ tests/test_logic.py
@@ -1,5 +1,5 @@
 from knight.logic import Solver, ChessBoard
-from knight.data_model import Statement, ChessCoordinate
+from knight.data_model import Statement, ChessCoordinate, Pair
 
 
 class TestChessBoard:
@@ -8,6 +8,27 @@ class TestChessBoard:
         assert len(ChessBoard.KNIGHT_MOVES) == 8
         assert len(set(ChessBoard.KNIGHT_MOVES)) == 8  # all distinct
         assert all(abs(x) in {1, 2} and abs(y) in {1, 2} for x, y in ChessBoard.KNIGHT_MOVES)
+
+    def test_move_from(self):
+        board = ChessBoard(size=8)
+        assert board.move_from(ChessCoordinate(letter='A', number=1), Pair(-1, 0)) is None
+        assert board.move_from(ChessCoordinate(letter='A', number=1), Pair(0, -1)) is None
+        assert board.move_from(ChessCoordinate(letter='A', number=1), Pair(-1, -1)) is None
+
+        assert board.move_from(ChessCoordinate(letter='A', number=8), Pair(-1, 0)) is None
+        assert board.move_from(ChessCoordinate(letter='A', number=8), Pair(0, 1)) is None
+        assert board.move_from(ChessCoordinate(letter='A', number=8), Pair(-1, 1)) is None
+
+        assert board.move_from(ChessCoordinate(letter='H', number=8), Pair(1, 0)) is None
+        assert board.move_from(ChessCoordinate(letter='H', number=8), Pair(0, 1)) is None
+        assert board.move_from(ChessCoordinate(letter='H', number=8), Pair(1, 1)) is None
+
+        assert board.move_from(ChessCoordinate(letter='H', number=1), Pair(1, 0)) is None
+        assert board.move_from(ChessCoordinate(letter='H', number=1), Pair(0, -1)) is None
+        assert board.move_from(ChessCoordinate(letter='H', number=1), Pair(1, -1)) is None
+
+        assert board.move_from(
+            ChessCoordinate(letter='B', number=1), Pair(1, 1)) == ChessCoordinate(letter='C', number=2)
 
 
 class TestSolver:
